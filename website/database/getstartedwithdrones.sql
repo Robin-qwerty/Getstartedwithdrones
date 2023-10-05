6789867e60007427c0894f3b83c285b5cbe47ada
Robin-qwerty <robin@humilis.net>
2023-10-05 23:13:40 +0200

    database update and image added

--- website/database/getstartedwithdrones.sql
+++ website/database/getstartedwithdrones.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 23, 2023 at 11:12 PM
+-- Generation Time: Oct 04, 2023 at 08:38 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -80,7 +80,7 @@ CREATE TABLE `fpv_prebuilds` (
   `blackbox` varchar(55) DEFAULT NULL,
   `fc_usb_plug` tinyint(1) DEFAULT NULL COMMENT '0=none\r\n1=micro-usb\r\n2=usb-c',
   `esc` varchar(128) DEFAULT NULL,
-  `power_input` varchar(128) DEFAULT NULL COMMENT 'power input of the esc',
+  `power_input` varchar(128) DEFAULT NULL COMMENT 'Recommended battery cell count(S) and type',
   `vtx` varchar(128) DEFAULT NULL,
   `camera` varchar(128) DEFAULT NULL,
   `antenna` varchar(128) DEFAULT NULL COMMENT 'antenna of the vtx',
@@ -94,17 +94,20 @@ CREATE TABLE `fpv_prebuilds` (
   `strap` varchar(128) DEFAULT NULL COMMENT 'battery strap',
   `led_lighting` varchar(128) DEFAULT NULL,
   `battery_conector` varchar(45) DEFAULT NULL,
-  `other` varchar(255) DEFAULT NULL
+  `other` varchar(255) DEFAULT NULL,
+  `beginner_friendly` int(2) DEFAULT NULL COMMENT '0=not_beginner_friendly\r\n10=beginner_friendly'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `fpv_prebuilds`
 --
 
-INSERT INTO `fpv_prebuilds` (`id`, `fpv_prebuild_name`, `manufacturer`, `description`, `drone_class`, `frame`, `arms`, `wheelbase`, `prop_duct`, `fc`, `micro_controler`, `imu`, `bluetooth_ble`, `osd_chip`, `blackbox`, `fc_usb_plug`, `esc`, `power_input`, `vtx`, `camera`, `antenna`, `receiver`, `buzzer`, `motors`, `props`, `gps`, `weight`, `length_width_height`, `strap`, `led_lighting`, `battery_conector`, `other`) VALUES
-(1, 'DJI FPV', '', 'Combines ease of use with FPV excitement.', 3, 'DJI Frame', 4, 200, 0, 'DJI FC', 'DJI Microcontroller', 'DJI IMU', 'Yes', 'DJI OSD Chip', 'Yes', 2, 'DJI ESC', '6S LiPo', 'DJI VTX', 'DJI Camera', 'DJI Antenna', 'DJI Receiver', 'Yes', 'DJI Motors', 'DJI Props', 'Yes', '750g', '200×200×50', 'DJI Strap', 'Yes', 'XT60', NULL),
-(2, 'iFlight Nazgul5', '', 'Known for durability and performance.', 3, 'iFlight Frame', 5, 250, 0, 'iFlight FC', 'iFlight Microcontroller', 'iFlight IMU', 'No', 'iFlight OSD Chip', 'Yes', 2, 'iFlight ESC', '4S LiPo', 'iFlight VTX', 'iFlight Camera', 'iFlight Antenna', 'iFlight Receiver', 'No', 'iFlight Motors', 'iFlight Props', 'No', '300g', '250×250×60', 'iFlight Strap', 'No', 'XT60', NULL),
-(3, 'EMAX Tinyhawk II', '', 'Micro FPV drone for indoor and outdoor flying.', 6, 'EMAX Frame', 2, 75, 1, 'EMAX FC', 'EMAX Microcontroller', 'EMAX IMU', 'No', 'EMAX OSD Chip', 'No', 1, 'EMAX ESC', '1S LiPo', 'EMAX VTX', 'EMAX Camera', 'EMAX Antenna', 'EMAX Receiver', 'Yes', 'EMAX Motors', 'EMAX Props', 'No', '32g', '75×75×25', 'EMAX Strap', 'No', 'PH2.0', NULL);
+INSERT INTO `fpv_prebuilds` (`id`, `fpv_prebuild_name`, `manufacturer`, `description`, `drone_class`, `frame`, `arms`, `wheelbase`, `prop_duct`, `fc`, `micro_controler`, `imu`, `bluetooth_ble`, `osd_chip`, `blackbox`, `fc_usb_plug`, `esc`, `power_input`, `vtx`, `camera`, `antenna`, `receiver`, `buzzer`, `motors`, `props`, `gps`, `weight`, `length_width_height`, `strap`, `led_lighting`, `battery_conector`, `other`, `beginner_friendly`) VALUES
+(1, 'DJI FPV', '', 'Combines ease of use with FPV excitement.', 3, 'DJI Frame', 4, 200, 0, 'DJI FC', 'DJI Microcontroller', 'DJI IMU', 'Yes', 'DJI OSD Chip', 'Yes', 2, 'DJI ESC', '6S LiPo', 'DJI VTX', 'DJI Camera', 'DJI Antenna', 'DJI Receiver', 'Yes', 'DJI Motors', 'DJI Props', 'Yes', '750g', '200×200×50', 'DJI Strap', 'Yes', 'XT60', NULL, NULL),
+(2, 'iFlight Nazgul5', '', 'Known for durability and performance.', 3, 'iFlight Frame', 5, 250, 0, 'iFlight FC', 'iFlight Microcontroller', 'iFlight IMU', 'No', 'iFlight OSD Chip', 'Yes', 2, 'iFlight ESC', '4S LiPo', 'iFlight VTX', 'iFlight Camera', 'iFlight Antenna', 'iFlight Receiver', 'No', 'iFlight Motors', 'iFlight Props', 'No', '300g', '250×250×60', 'iFlight Strap', 'No', 'XT60', NULL, NULL),
+(3, 'EMAX Tinyhawk II', '', 'Micro FPV drone for indoor and outdoor flying.', 6, 'EMAX Frame', 2, 75, 1, 'EMAX FC', 'EMAX Microcontroller', 'EMAX IMU', 'No', 'EMAX OSD Chip', 'No', 1, 'EMAX ESC', '1S LiPo', 'EMAX VTX', 'EMAX Camera', 'EMAX Antenna', 'EMAX Receiver', 'Yes', 'EMAX Motors', 'EMAX Props', 'No', '32g', '75×75×25', 'EMAX Strap', 'No', 'PH2.0', NULL, NULL),
+(5, 'Diatone Roma F5 V2 Analog 4S PNP', 'Diatone', 'The Diatone Roma F5 V2 Analog is a 5\" Freestyle Quadcopter that comes with the RunCam Phoenix 2 - Mamba Edition camera and has a KV option of 2450KV (4S) to best fit your FPV needs. Equipped with quality MAMBA electronics and Analog video, this quad is great for freestyle ripping! Features Titanium Steel metal parts at the front of the frame, RunCam Phoenix 2 - Mamba Edition Camera, Injection-molded colorful protection parts, Slammed configuration, Increased arm strength.', 2, NULL, NULL, NULL, NULL, 'MAMBA APP F722 MK1', NULL, NULL, NULL, NULL, NULL, 2, 'MAMBA F50 _BL32', '4S LiPo', 'MAMBA Ultra 1000MW', 'RUNCAM PHOENIX2', NULL, NULL, NULL, 'MAMBA TOKA 2306.5 2450KV', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(6, 'Diatone Roma F5 V2 Analog 6S PNP', 'Diatone', 'The Diatone Roma F5 V2 Analog is a 5\" Freestyle Quadcopter that comes with the RunCam Phoenix 2 - Mamba Edition camera and has a KV option of 1770KV (6S) to best fit your FPV needs. Equipped with quality MAMBA electronics and Analog video, this quad is great for freestyle ripping! Features Titanium Steel metal parts at the front of the frame, RunCam Phoenix 2 - Mamba Edition Camera, Injection-molded colorful protection parts, Slammed configuration, Increased arm strength.', 2, NULL, NULL, NULL, NULL, 'MAMBA APP F722 MK1', NULL, NULL, NULL, NULL, NULL, 2, 'MAMBA F50BL32', '6S LiPo', 'MAMBA Ultra 1000MW', 'RUNCAM PHOENIX2', NULL, NULL, NULL, 'MAMBA TOKA 2306.5 1770KV', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -179,9 +182,9 @@ CREATE TABLE `transmitters` (
 --
 
 INSERT INTO `transmitters` (`id`, `transmitter_name`, `manufacturer`, `description`, `weight`, `buildin_rf`, `supported_protocols`, `frequency`, `rf_power`, `rf_channels`, `control_distance`, `antenna_gain`, `operation_voltage`, `operation_current`, `operating_system`, `firmware_upgrade_method`, `display`, `speaker`, `battery`, `battery_type`, `charching`, `ports`, `gimbal`, `module_bay`, `length_width_height`, `built_material`, `user_manual`, `other`) VALUES
-(1, 'Radiomaster TX16S', '', 'The Radiomaster TX16S is a versatile and popular FPV transmitter designed for drone enthusiasts. It comes with a variety of advanced features and customization options.', '980 grams', 'Built-in RF: FrSky R9M Lite Pro', 'Supported Protocols: FrSky, ELRS, Crossfire', '2.4GHz', '100mW', '16', 'Up to 1,000 meters', 'Antenna Gain: 2dBi', 'Operation Voltage: 7.4V', 'Operation Current: 350mA', 'Operating System: OpenTX', 'Firmware Upgrade Method: USB-C', '3.5-inch color touchscreen', 'Built-in speaker and audio jack', '4,000mAh Li-ion battery', 'Battery Type: Lithium-ion', 'Charging: USB-C', 'Ports: Micro USB, Headphone jack', 'Hall effect gimbals ', 'Module Bay: Yes', 'Dimensions: 180mm x 190mm x 58mm', 'Built Material: Plastic and Metal', 'User Manual: https://example.com/tx16s-user-manual.pdf', NULL),
-(2, 'Radiomaster TX12', '', 'The Radiomaster TX12 is an excellent entry-level FPV transmitter designed for drone enthusiasts. It offers a balance between features and affordability.', '680 grams', 'Built-in RF: FrSky R9M Lite Pro', 'Supported Protocols: FrSky, ELRS', '2.4GHz', '100mW', '16', 'Up to 800 meters', 'Antenna Gain: 2dBi', 'Operation Voltage: 7.4V', 'Operation Current: 250mA', 'Operating System: OpenTX', 'Firmware Upgrade Method: USB-C', '2.8-inch color touchscreen', 'Built-in speaker and audio jack', '2,500mAh Li-ion battery', 'Battery Type: Lithium-ion', 'Charging: USB-C', 'Ports: Micro USB, Headphone jack', 'Hall effect gimbals ', 'Module Bay: No', 'Dimensions: 170mm x 180mm x 52mm', 'Built Material: Plastic', 'User Manual: https://example.com/tx12-user-manual.pdf', NULL),
-(3, 'Radiomaster Zoro', '', 'The Radiomaster Zoro is a high-end FPV transmitter designed for professional drone pilots. It offers advanced features and precision control.', '730 grams', 'Built-in RF: FrSky R9M Lite Pro', 'Supported Protocols: FrSky, ELRS', '2.4GHz', '500mW', '24', 'Up to 1,000 meters', 'Antenna Gain: 3dBi', 'Operation Voltage: 7.4V', 'Operation Current: 300mA', 'Operating System: OpenTX', 'Firmware Upgrade Method: SD Card', '4.3-inch color touchscreen', 'Built-in speaker and audio jack', '3,000mAh LiPo battery', 'Battery Type: Lithium Polymer', 'Charging: USB-C', 'Ports: USB-C, Headphone jack', 'Hall effect gimbals ', 'Module Bay: Yes, JR bay', 'Dimensions: 190mm x 200mm x 58mm', 'Built Material: Aluminum alloy and plastic', 'User Manual: https://example.com/zoro-user-manual.pdf', NULL),
+(1, 'Radiomaster TX16S', '', 'The Radiomaster TX16S is a versatile and popular FPV transmitter designed for drone enthusiasts. It comes with a variety of advanced features and customization options.', '980 grams', 'FrSky R9M Lite Pro', 'FrSky, ELRS, Crossfire', '2.4GHz', '100mW', '16', 'Up to 1,000 meters', '2dBi', '7.4V', '350mA', 'OpenTX', 'USB-C', '3.5-inch color touchscreen', 'Built-in speaker and audio jack', '4,000mAh Li-ion battery', 'Lithium-ion', 'USB-C', 'Micro USB, Headphone jack', 'Hall effect gimbals ', 'Yes', '180mm x 190mm x 58mm', 'Plastic and Metal', 'https://example.com/tx16s-user-manual.pdf', NULL),
+(2, 'Radiomaster TX12', '', 'The Radiomaster TX12 is an excellent entry-level FPV transmitter designed for drone enthusiasts. It offers a balance between features and affordability.', '680 grams', 'FrSky R9M Lite Pro', 'FrSky, ELRS', '2.4GHz', '100mW', '16', 'Up to 800 meters', '2dBi', '7.4V', '250mA', 'OpenTX', 'USB-C', '2.8-inch color touchscreen', 'Built-in speaker and audio jack', '2,500mAh Li-ion battery', 'Lithium-ion', 'USB-C', 'Micro USB, Headphone jack', 'Hall effect gimbals ', 'No', '170mm x 180mm x 52mm', 'Plastic', 'https://example.com/tx12-user-manual.pdf', NULL),
+(3, 'Radiomaster Zoro', '', 'The Radiomaster Zoro is a high-end FPV transmitter designed for professional drone pilots. It offers advanced features and precision control.', '730 grams', 'FrSky R9M Lite Pro', 'FrSky, ELRS', '2.4GHz', '500mW', '24', 'Up to 1,000 meters', '3dBi', '7.4V', '300mA', 'OpenTX', 'SD Card', '4.3-inch color touchscreen', 'Built-in speaker and audio jack', '3,000mAh LiPo battery', 'Lithium Polymer', 'USB-C', 'USB-C, Headphone jack', 'Hall effect gimbals ', 'Yes, JR bay', '190mm x 200mm x 58mm', 'Aluminum alloy and plastic', 'https://example.com/zoro-user-manual.pdf', NULL),
 (4, 'FrSky Taranis X9D Plus', '', 'Reliable and feature-rich transmitter', '880g', 'Yes', 'FrSky ACCST', '2.4GHz', '100mW', '16', 'Over 1km', 'N/A', '7.2V', '260mA', 'OpenTX', 'SD Card', 'Yes', 'Yes', '2000mAh', 'NiMH', 'Yes', 'USB, SD Card', 'Hall effect gimbals ', 'Yes', '190x215x85mm', 'Plastic', 'FrSky Taranis X9D Plus Manual', NULL),
 (5, 'Flysky Nirvana NV14', '', 'Ergonomic transmitter with good build quality', '700g', 'Yes', 'AFHDS 2A', '2.4GHz', '100mW', '10', 'Over 1km', 'N/A', '7.4V', '260mA', 'OpenTX', 'USB', 'Yes', 'Yes', '2200mAh', 'LiPo', 'Yes', 'USB', 'Hall effect gimbals ', 'Yes', '200x95x185mm', 'Plastic', 'Flysky Nirvana NV14 Manual', NULL),
 (6, 'Jumper T-Lite', '', 'Compact and affordable transmitter', '250g', 'Yes', 'Multi-protocol', '2.4GHz', '100mW', '4', 'Over 1km', 'N/A', '7.4V', '260mA', 'OpenTX', 'USB', 'No', 'No', '1000mAh', 'LiPo', 'Yes', 'USB', 'Hall effect gimbals ', 'No', '170x159x108mm', 'Plastic', 'Jumper T-Lite Manual', NULL),
@@ -236,7 +239,7 @@ ALTER TABLE `fpv_goggles`
 -- AUTO_INCREMENT for table `fpv_prebuilds`
 --
 ALTER TABLE `fpv_prebuilds`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `logs`
